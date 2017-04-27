20fd8f34a645dab150d2600f9a93d2488f3ca2d8
Demon000 <demonsingur@gmail.com>
2017-04-27 09:10:59 +0300

    AOSP-ify

--- aosp_libra.mk
+++ aosp_libra.mk
@@ -6,16 +6,16 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/libra/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_libra
+PRODUCT_NAME := aosp_libra
 PRODUCT_DEVICE := libra
 
 PRODUCT_BRAND := Xiaomi
